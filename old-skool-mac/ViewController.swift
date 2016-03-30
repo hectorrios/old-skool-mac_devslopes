92b885c72024502b88641fdb9863d390b987594f
Hector Rios <oj.rios@gmail.com>
2016-03-30 11:07:33 +0200

    complete exercise

--- old-skool-mac/ViewController.swift
+++ old-skool-mac/ViewController.swift
@@ -10,9 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainLbl: UILabel!
+    
+    var phrases = ["Booting from floppy...\n", "Reading from disk...\n", "Updating the registry...\n", ".........\n", "............\n",
+    "..............................\n", "Welcome Jessica, \n It is nice to see you again"]
+    
+    //MARK: Lifecycle functions
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        mainLbl.text = ""
+
+//        for var x=0; x < phrases.count; x++ {
+//            var txt = mainLbl.text!
+//            txt += phrases[x]
+//            mainLbl.text = txt
+//        }
+        
+//        var x = 0
+//        repeat {
+//            var txt = mainLbl.text!
+//            txt += phrases[x]
+//            mainLbl.text = txt
+//            x++
+//        } while x < phrases.count
+
+        for phrase in phrases {
+            var txt = mainLbl.text!
+            txt += phrase
+            mainLbl.text = txt
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
